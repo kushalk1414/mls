7ece8186b590b58ebfb3ef4be1f58c31f318003c
kushal karamchandani <kushal.karamchandani@nordakademie.de>
2026-03-09 20:10:20 +0100

    added basic functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use std::{fs, path::PathBuf, path::Path};
+use clap::Parser;
+use owo_colors::OwoColorize;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    path: Option<PathBuf>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let path = cli.path.unwrap_or(PathBuf::from("."));
+
+    if let Ok(does_exist) = fs::exists(&path) {
+        if does_exist {
+            get_files(&path).iter()
+            .for_each(|f| println!("{f}"));
+
+        } else {
+            println!("{}", "provided path does not exist".red())
+        }
+    } else {
+            println!("{}", "error reading directory".red())
+    }
+
+    println!()
+}
+
+fn get_files(path: &Path) -> Vec<String> {
+    let mut data = Vec::default();
+    if let Ok(read_dir) = fs::read_dir(path) {
+        for entry in read_dir {
+            if let Ok(file) = entry {
+                data.push(file.file_name()
+                .into_string()
+                .unwrap_or("unknown".into())
+            )
+            }
+        }
+    }
+    data
+}
+
+#[derive(Debug)]
+enum EntryType {
+    File,
+    Dir
+}
+
+#[derive(Debug)]
+struct FileMetadata{
+    name: String,
+    _type: EntryType,
+    len_bytes: u64,
+    modified: String
 }
