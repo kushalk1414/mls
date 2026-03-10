ac5fdaa3a31762217ddc2a74948405344617789a
kushal karamchandani <kushal.karamchandani@nordakademie.de>
2026-03-10 16:57:54 +0100

    added more file information

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{fs, path::PathBuf, path::Path};
+use std::{fs::{self, File}, path::{Path, PathBuf}};
 use clap::Parser;
 use owo_colors::OwoColorize;
+use chrono::{DateTime, Utc};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -15,7 +16,7 @@ fn main() {
     if let Ok(does_exist) = fs::exists(&path) {
         if does_exist {
             get_files(&path).iter()
-            .for_each(|f| println!("{f}"));
+            .for_each(|f| println!("{:#?}", f));
 
         } else {
             println!("{}", "provided path does not exist".red())
@@ -27,15 +28,33 @@ fn main() {
     println!()
 }
 
-fn get_files(path: &Path) -> Vec<String> {
+fn get_files(path: &Path) -> Vec<FileMetadata> {
     let mut data = Vec::default();
     if let Ok(read_dir) = fs::read_dir(path) {
         for entry in read_dir {
             if let Ok(file) = entry {
-                data.push(file.file_name()
-                .into_string()
-                .unwrap_or("unknown".into())
-            )
+                if let Ok(metadata) = fs::metadata(&path) {
+                    let file_info = FileMetadata {
+                        name: file.file_name()
+                            .into_string()
+                            .unwrap_or("unknown".into()),
+                        _type: if metadata.is_dir() {
+                            EntryType::Dir
+                        } else {
+                            EntryType::File
+                        },
+                        len_bytes: metadata.len(),
+                        modified: if let Ok(modi) = metadata.modified() {
+                            let date: DateTime<Utc> = modi.into();
+                            format!("{}", date.format("%a %b %e %Y"))
+                        } else {
+                            String::default()
+                        }
+                    };
+                    data.push(file_info);
+                }
+
+                
             }
         }
     }
