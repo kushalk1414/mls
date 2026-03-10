a5e871abdac548dcc624752d022c4ede915e625b
kushal karamchandani <kushal.karamchandani@nordakademie.de>
2026-03-10 17:26:53 +0100

    added a little styling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::{fs::{self, File}, path::{Path, PathBuf}};
+use std::{fs::{self}, path::{Path, PathBuf}, fmt};
 use clap::Parser;
-use owo_colors::OwoColorize;
+use owo_colors::{OwoColorize};
 use chrono::{DateTime, Utc};
+use tabled::{Table, Tabled, settings::{Color, Style, object::{Column, Columns, Rows}}};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -12,20 +13,26 @@ fn main() {
     let cli = Cli::parse();
 
     let path = cli.path.unwrap_or(PathBuf::from("."));
-
+    
     if let Ok(does_exist) = fs::exists(&path) {
         if does_exist {
-            get_files(&path).iter()
-            .for_each(|f| println!("{:#?}", f));
-
+            let files = get_files(&path);
+            let mut file_table = Table::new(files);
+            file_table.with(Style::rounded());
+            file_table.modify(Columns::first(), Color::FG_CYAN);
+            file_table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+            println!("{file_table}");
+            
         } else {
             println!("{}", "provided path does not exist".red())
         }
     } else {
-            println!("{}", "error reading directory".red())
+        println!("{}", "error reading directory".red())
     }
 
-    println!()
+
+
+
 }
 
 fn get_files(path: &Path) -> Vec<FileMetadata> {
@@ -33,17 +40,19 @@ fn get_files(path: &Path) -> Vec<FileMetadata> {
     if let Ok(read_dir) = fs::read_dir(path) {
         for entry in read_dir {
             if let Ok(file) = entry {
-                if let Ok(metadata) = fs::metadata(&path) {
+                if let Ok(metadata) = fs::metadata(&file.path()) {
                     let file_info = FileMetadata {
                         name: file.file_name()
                             .into_string()
                             .unwrap_or("unknown".into()),
                         _type: if metadata.is_dir() {
                             EntryType::Dir
-                        } else {
+                        } else if metadata.is_file(){
                             EntryType::File
+                        } else {
+                            EntryType::Unknown
                         },
-                        len_bytes: metadata.len(),
+                        size: metadata.len(),
                         modified: if let Ok(modi) = metadata.modified() {
                             let date: DateTime<Utc> = modi.into();
                             format!("{}", date.format("%a %b %e %Y"))
@@ -64,13 +73,24 @@ fn get_files(path: &Path) -> Vec<FileMetadata> {
 #[derive(Debug)]
 enum EntryType {
     File,
-    Dir
+    Dir,
+    Unknown
 }
 
-#[derive(Debug)]
+impl fmt::Display for EntryType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EntryType::File => write!(f, "File"),
+            EntryType::Dir => write!(f, "Dir"),
+            EntryType::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
+
+#[derive(Debug, Tabled)]
 struct FileMetadata{
     name: String,
     _type: EntryType,
-    len_bytes: u64,
+    size: u64,
     modified: String
 }
